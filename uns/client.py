35f62dd6db2a0b00e62bc834e5e24d30a96228f7
Bert JW Regeer <bertjw@regeer.org>
2014-09-01 23:16:57 -0600

    Add quick command that lists users sites

--- uns/client.py
+++ uns/client.py
@@ -36,7 +36,41 @@ def cli(ctx, **kw):
     ctx.obj['config_path'] = config_path
     ctx.obj['config'] = config
 
+@click.group()
+@click.pass_context
+def sites(ctx):
+    pass
+
+@click.command(name='list')
+@click.pass_context
+def sites_list(ctx):
+    click.echo('Listing all sites...')
+
+    try:
+        import requests
+
+        headers = {
+                'x-api-ticket': ctx.obj['config'].get('DEFAULT', 'api_ticket')
+                }
+
+        r = requests.get(ctx.obj['config'].get('DEFAULT', 'api_url'), headers=headers)
+
+        if r.status_code == 200:
+            sites = r.json()['sites']
+
+            for site in sites:
+                outlines = '{id}: {title}\r\n\t{tagline}'.format(**site)
+                click.echo(outlines)
+        else:
+            click.echo('Failed to fetch list of sites.')
+    except:
+        click.echo('Failed to connect to server to get list of sites')
+
+
+sites.add_command(sites_list)
+
 cli.add_command(setup)
+cli.add_command(sites)
 
 def main():
     """This starts the client...
