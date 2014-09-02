dceb6cb0b32fee01d512134f6f6ddfeea3720a02
Bert JW Regeer <bertjw@regeer.org>
2014-09-01 23:00:57 -0600

    Add more options for running non-interactive
    This is a use case I want to support, and with the previous way things
    were done it was not possible because of the prompts. This removes the
    prompts unless running interactive.

--- uns/setup.py
+++ uns/setup.py
@@ -17,6 +17,9 @@ import click
 default_api_url = 'https://api.usingnamespace.com/v1/'
 
 @click.command()
+@click.option('--interactive/--no-interactive', default=True, help="Run this script interactively")
+@click.option('--test/--no-test', default=True, help="Test the new configuration (non-interactive only)")
+@click.option('--save/--no-save', default=False, help="Save the new configuration (non-interactive only)")
 @click.option('--api-url', default=default_api_url, help="The API URL")
 @click.option('--api-ticket', help="The API ticket")
 @click.pass_context
@@ -25,11 +28,15 @@ def setup(ctx, **kw):
     config_defaults = {}
     config = SafeConfigParser(config_defaults)
 
+    interactive = kw['interactive']
+    test = kw['test']
+    save = kw['save']
+
     if os.path.exists(config_path):
         config.readfp(open(config_path))
         
         try:
-            if kw['api_url'] == default_api_url:
+            if kw['api_url'] == default_api_url and interactive:
                 kw['api_url'] = config.get('DEFAULT', 'api_url')
 
             if kw['api_ticket'] is None:
@@ -37,12 +44,12 @@ def setup(ctx, **kw):
         except NoOptionError:
             pass
 
-    api_url_prompt = 'What api URL would you like to use'
+    api_url_prompt = 'What API URL would you like to use'
 
     if kw['api_url'] != default_api_url:
         api_url_prompt = api_url_prompt + ' (Default: {})'.format(default_api_url)
-   
-    api_url = click.prompt(api_url_prompt, default=kw['api_url'])
+    
+    api_url = kw['api_url'] if not interactive else click.prompt(api_url_prompt, default=kw['api_url'])
     api_ticket = kw['api_ticket'] if not interactive else click.prompt('API ticket for your account', default=kw['api_ticket'])
 
     if not interactive and api_ticket is None:
@@ -52,12 +59,11 @@ def setup(ctx, **kw):
     config.set('DEFAULT', 'api_url', api_url)
     config.set('DEFAULT', 'api_ticket', api_ticket)
 
-    if click.confirm('Test new configuration'):
+    if (not interactive and test) or (interactive and click.confirm('Test new configuration')):
         click.echo('Testing configuration...')
 
-        click.echo('Tests passed, safe to save this config file')
 
-    if click.confirm('Save the new configuration file?', abort=True):
+    if (not interactive and save) or (interactive and click.confirm('Save the new configuration file?', abort=True, default=save)):
         try:
             os.makedirs(os.path.abspath(ctx.obj['config_dir']), 0o700)
         except os.error:
